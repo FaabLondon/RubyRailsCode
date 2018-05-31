ade111401c8085ab3674fddb183da14fad22f3a9
FaabLondon <faabke@gmail.com>
2018-05-31 09:21:39 +0000

    added reference to review in comment table and added add_index

--- db/migrate/20180531083856_create_comments.rb
+++ db/migrate/20180531083856_create_comments.rb
@@ -5,7 +5,12 @@ class CreateComments < ActiveRecord::Migration[5.1]
       t.text :comment
       t.integer :review_id
 
+      t.references :review, foreign_key: true
+
       t.timestamps
     end
+    
+    add_index :comments, [:review_id, :created_at]
+    
   end
 end
